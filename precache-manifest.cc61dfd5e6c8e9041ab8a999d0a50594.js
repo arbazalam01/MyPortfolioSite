1c6e02c827ed1f3770349d1334da68c07fd7d203
arbazalam01 <drarbazalam@gmail.com>
2022-07-17 12:55:43 +0530

    Updates

---
+++ precache-manifest.cc61dfd5e6c8e9041ab8a999d0a50594.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d231f28193a035d548a82ac2f1239559",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53f5372bde55278075da",
+    "url": "/static/css/main.d0035590.chunk.css"
+  },
+  {
+    "revision": "d8ac6e385638975a1dc3",
+    "url": "/static/js/2.9704892c.chunk.js"
+  },
+  {
+    "revision": "64f54ce285484345cd5e3bd96b120148",
+    "url": "/static/js/2.9704892c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53f5372bde55278075da",
+    "url": "/static/js/main.411a6283.chunk.js"
+  },
+  {
+    "revision": "8d1e58fe4777607b2d16",
+    "url": "/static/js/runtime-main.18a4fe7b.js"
+  },
+  {
+    "revision": "d7fa95f957157cdd663063f3c6b2be59",
+    "url": "/static/media/7038048-purple-dark-smoke.d7fa95f9.jpg"
+  },
+  {
+    "revision": "98295dd756dc928e3baaf0aeeefde70e",
+    "url": "/static/media/Covid.98295dd7.jpg"
+  },
+  {
+    "revision": "da88a5224aaf90202771502983ac2ad9",
+    "url": "/static/media/DomainSearch.da88a522.jpg"
+  },
+  {
+    "revision": "335977714682f254f03e25969180575a",
+    "url": "/static/media/Resume.33597771.pdf"
+  },
+  {
+    "revision": "d12bb331f2d6c80d41861ce9c6e688bb",
+    "url": "/static/media/SnakeGame.d12bb331.jpg"
+  },
+  {
+    "revision": "6b79a54237b6e301ba9399a0a31a5400",
+    "url": "/static/media/TypingSpeed.6b79a542.jpg"
+  },
+  {
+    "revision": "3fca799383e7877d28571022d35b50b8",
+    "url": "/static/media/arbaz_photo.3fca7993.png"
+  }
+]);
